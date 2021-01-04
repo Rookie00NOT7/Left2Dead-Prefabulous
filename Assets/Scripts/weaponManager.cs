e4b577bc1e3eb7f1d8728d54a058d1f5c754db36
fadi-saad <fadoxin@gmail.com>
2021-01-04 12:34:45 +0200

    added shotgun to the weapon manager

--- Assets/Scripts/weaponManager.cs
+++ Assets/Scripts/weaponManager.cs
@@ -4,35 +4,39 @@ using UnityEngine;
 
 public class weaponManager : MonoBehaviour
 {
-    private bool[] weapons = {true, true, false, false};
+    private bool[] weapons = { true, true, true, false, false };
     private int current = 0;
     private GameObject pistol;
     private GameObject SGM;
-//    to be made
-//    private GameObject shotgun;
-//    private GameObject assault;
+    private GameObject shotgun;
+    //    to be made
+    //    private GameObject assault;
 
     void Start()
     {
         pistol = GameObject.FindGameObjectWithTag("playerGun");
         SGM = GameObject.FindGameObjectWithTag("playerSGM");
+        shotgun = GameObject.FindGameObjectWithTag("Shotty");
         SGM.SetActive(false);
+        shotgun.SetActive(false);
     }
 
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.C))
         {
-            current = (current + 1) % 4;
+            current = (current + 1) % 5;
             while (!weapons[current])
             {
-                current = (current + 1) % 4;
+                current = (current + 1) % 5;
             }
             print(current);
-            switch (current) {
-                case 0: pistol.SetActive(true); SGM.SetActive(false); break;
-                case 1: pistol.SetActive(false); SGM.SetActive(true); break;
-                //the rest
+            switch (current)
+            {
+                case 0: pistol.SetActive(true); SGM.SetActive(false); shotgun.SetActive(false); break;
+                case 1: pistol.SetActive(false); SGM.SetActive(true); shotgun.SetActive(false); break;
+                case 2: pistol.SetActive(false); SGM.SetActive(false); shotgun.SetActive(true); break;
+                    //the rest
             }
         }
     }
