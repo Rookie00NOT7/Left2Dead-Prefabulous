6d3bcf16d991a0c285a6268e7da3353432be4964
Omar-Handouk <omar.handouk@yandex.com>
2021-01-10 00:21:55 +0200

    Added GameManager Prefab and Script

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+   public static GameManager Instance;
+
+   [HideInInspector]
+   public bool ellieSelected = true;
+
+   private void Awake() {
+      if (Instance == null) {
+          Instance = this;
+          DontDestroyOnLoad(this.gameObject);
+      } else {
+          Destroy(this);
+      }
+   }
+}
