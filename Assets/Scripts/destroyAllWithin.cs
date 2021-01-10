efc4e5ec724c1ac9d67d177810cf9dcab4d745cb
fadi-saad <fadoxin@gmail.com>
2021-01-10 18:24:39 +0200

    added no go back behavior from level to level, modified pipe bombs to affect only nearby enemies, changed the shotgun listner button to fix the ctrl problem, plus some small changes in scene

---
+++ Assets/Scripts/destroyAllWithin.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class destroyAllWithin : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "target" || other.tag == "spitter" || other.tag == "charger" || other.tag == "Tank" || other.tag == "boomer")
+        {
+            Destroy(other.gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/levelDestroyer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class levelDestroyer : MonoBehaviour
+{
+    public GameObject levelDest;
+    public GameObject closed1;
+    public GameObject closed2;
+    public GameObject opened1;
+    public GameObject opened2;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            levelDest.SetActive(true);
+            closed1.SetActive(true);
+            closed2.SetActive(true);
+            opened1.SetActive(false);
+            opened2.SetActive(false);
+            //next objective card UI
+        }
+    }
+}
