9649c7c91e614bc894860569bcb55ade8fa1a24d
Omar-Handouk <omar.handouk@yandex.com>
2021-01-12 22:25:33 +0200

    Feature audio mixer & Soundtrack manager
    This commit contains the audio mixer and soundtrack manager prefabs.
    
    Change Logs:
    - Modified Prefabs to have output source to Audio Mixer.
    - Added Audio Mixer Prefab
    - Added Soundtrack Manager Prefab.

---
+++ Assets/Scripts/SoundtrackManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundtrackManager : MonoBehaviour
+{
+   public AudioClip calmTrack;
+   public AudioClip upbeatTrack;
+
+   private AudioSource audioSource;
+
+   private void Start() {
+       audioSource = gameObject.GetComponent<AudioSource>();
+   }
+
+   public void PlayCalmTrack() {
+       audioSource.clip = calmTrack;
+       audioSource.Play();
+   }
+
+   public void PlayUpbeatTrack() {
+       audioSource.clip = upbeatTrack;
+       audioSource.Play();
+   }
+
+   public void PauseTrack() {
+       audioSource.Pause();
+   }
+
+   public void StopTrack() {
+       audioSource.Stop();
+   }
+}
