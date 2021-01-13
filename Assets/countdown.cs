4353fc8f331f01c0e6637fc710c91e70396de891
fadi-saad <fadoxin@gmail.com>
2021-01-13 14:32:27 +0200

    added counter at the last level

---
+++ Assets/countdown.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class countdown : MonoBehaviour
+{
+    private float timeRemaining = 3f*60f;
+    private bool counting = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            counting = true;
+        }
+    }
+    public void stopCounting()
+    {
+        counting = false;
+    }
+    void Update()
+    {
+        if (counting)
+        {
+            timeRemaining -= Time.deltaTime;
+            float minutes = Mathf.FloorToInt(timeRemaining / 60);
+            float seconds = Mathf.FloorToInt(timeRemaining % 60);
+            Debug.Log(minutes + ":" + seconds);
+        }
+    }
+}
---
+++ Assets/endCountdown.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class endCountdown : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            GameObject.FindGameObjectWithTag("timer").GetComponent<countdown>().stopCounting();
+        }
+    }
+}
